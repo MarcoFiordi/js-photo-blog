50011b79cef1fa41fefd65a216a942202b2c39f6
Marco Fiordi <marco.fiordi97@gmail.com>
2026-04-18 14:45:30 +0200

    selezionati elementi DOM per le card, loading e error

--- script/script.js
+++ script/script.js
@@ -1,14 +1,11 @@
+// @ts-check
 'use strict';
 
-const API_URL = 'https://lanciweb.github.io/demo/api/pictures/';
+// Seleziono gli elementi del Dom
+const listaFotoElem = document.querySelector('#photo-list');
+const loadingMsgElem = document.querySelector('#loadingMsg');
+const errorMsgElem = document.querySelector('#errorMsg');
 
-fetch(API_URL)
-    .then(response => {
-        return response.json();
-    })
-    .then(json => {
-        console.log(json);
-    })
-    .catch(error => {
-        console.error(error);
-    });
+console.log(listaFotoElem);
+console.log(loadingMsgElem);
+console.log(errorMsgElem);
