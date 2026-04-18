627357162493ef6f6db9f37ebf32369cca5782fa
Marco Fiordi <marco.fiordi97@gmail.com>
2026-04-18 15:01:59 +0200

    Aggiunta fetch con gestione risposta JSON e commenti esplicativi

--- script/script.js
+++ script/script.js
@@ -1,11 +1,28 @@
 // @ts-check
 'use strict';
 
+const API_URL = 'https://lanciweb.github.io/demo/api/pictures/'
 // Seleziono gli elementi del Dom
 const listaFotoElem = document.querySelector('#photo-list');
 const loadingMsgElem = document.querySelector('#loadingMsg');
 const errorMsgElem = document.querySelector('#errorMsg');
 
-if (loadingMsgElem !== null){
+if (loadingMsgElem !== null) {
     loadingMsgElem.textContent = 'Caricamento...';
 }
+// Chiamata API
+fetch(API_URL)
+    // Quando arriva la risposta la trasformo in JSON
+    .then((response) => {
+        return response.json();
+    })
+    // Quando arrivano i dati veri, li passo alla funzione che crea le card
+    .then((fotoArray) => {
+        stampaCard(fotoArray);
+    })
+    // Se si presenta qualche errore, lo stampo in console
+    .catch((error) => {
+        console.error(error);
+    });
+
+    
